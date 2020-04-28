3c329027ea3a2fd81fe0e37e803b90408a006664
Freditansari <Freditansari@users.noreply.github.com>
2020-04-28 20:00:16 +0700

    added html interface for js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 let multer = require('multer');
 let cors = require('cors');
+const path = require('path');
+
 const port = 3000
 
 let storage = multer.diskStorage({
@@ -14,9 +16,31 @@ let storage = multer.diskStorage({
 
 })
 
-let upload = multer({storage: storage}).array('file');
+let upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).array('file');
 
-app.post('/upload',function(req, res) {
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /3gp|aa|aac|aax|act|aiff|alac|amr|ape|au|awb|dct|dss|dvf|flac|gsm|m4a|m4b|m4b|mmf|mp3|mpc|msv|nsf|ogg|oga|mogg|ra|rm|wav|wma|wv|webm/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Audio Only!');
+    // return res.status(500).json({message: "audio only"})
+  }
+}
+
+app.post('/upload',cors(), (req, res)=> {
     
     upload(req, res, function (err) {
      
